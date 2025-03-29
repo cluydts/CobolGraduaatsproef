3077ec8a1e8269daa55400158040214551535daa
cluydts <jeroencluydts1@gmai.com>
2025-03-29 19:56:40 +0100

    aanpassingen

--- MainProgram.cbl
+++ MainProgram.cbl
@@ -3,23 +3,25 @@
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-           01  PRINCIPAL        PIC 9(8)V99.
+           01  CAPITAL        PIC 9(8)V99.
            01  RATE             PIC 9(2)V99.
-           01  TIME             PIC 9(2).
+           01  Years             PIC 9(2).
            01  INTEREST         PIC 9(8)V99.
            01  TOTAL-AMOUNT     PIC 9(8)V99.
+           01  TOTAL-AMOUNT-DISPLAY PIC ZZZ,ZZZ,ZZ9.99.
 
        PROCEDURE DIVISION.
            DISPLAY "Voer het startkapitaal in: ".
-           ACCEPT PRINCIPAL.
+           ACCEPT CAPITAL.
            DISPLAY "Voer de rentevoet in (bijv. 5.5): ".
            ACCEPT RATE.
            DISPLAY "Voer de looptijd in jaren in: ".
-           ACCEPT TIME.
+           ACCEPT Years.
 
-           COMPUTE INTEREST = (PRINCIPAL * RATE * TIME) / 100.
-           COMPUTE TOTAL-AMOUNT = PRINCIPAL + INTEREST.
+           COMPUTE INTEREST = (CAPITAL * RATE * Years) / 100.
+           COMPUTE TOTAL-AMOUNT = CAPITAL + INTEREST.
+           move TOTAL-AMOUNT to TOTAL-AMOUNT-DISPLAY.
 
-           DISPLAY "Totale bedrag na " TIME " jaar: " TOTAL-AMOUNT.
+           DISPLAY "Totale bedrag na " Years " jaar: " TOTAL-AMOUNT-DISPLAY.
 
            STOP RUN.
