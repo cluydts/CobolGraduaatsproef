4df263447b75f74fc566221f93c08a44028a58ae
cluydts <jeroencluydts1@gmai.com>
2025-04-03 12:51:39 +0200

    testing

---
+++ testprogramma.cbl
@@ -0,0 +1,45 @@
+           identification division.
+           program-id. testprogramma.
+           environment division.
+           input-output section.
+           file-control.
+               select Invoerbestand assign to "Testbestand.txt"
+                   organization is line sequential.
+               select Uitvoerbestand assign to "UitvoerTestBestand.txt"
+                   organization is line sequential.
+
+           data division.
+           file section.
+           fd Invoerbestand.
+           01 Leesregel pic x(5).
+           01 EOF-Flag PIC 9 VALUE 0.
+
+           FD Uitvoerbestand.
+           01 UitvoerRegel PIC X(35).
+  
+           working-storage section.
+           01 Geconverteerd-Getal pic 9(5).
+           01 Resultaat pic 9(5).
+
+           procedure division.
+           open input Invoerbestand.
+           open output Uitvoerbestand.
+           perform until EOF-Flag = 1
+               read Invoerbestand into Leesregel
+               at end  
+                   move 1 to EOF-Flag
+               not at end
+                   move function numval(Leesregel) to Geconverteerd-Getal
+                   compute Resultaat = Geconverteerd-Getal * 2
+                   display "Origineel: " Leesregel " | verdubbeld: " Resultaat
+
+                   string "Origineel: " Leesregel " | verdubbeld: " Resultaat
+                       Resultaat delimited by size into UitvoerRegel
+                   write UitvoerRegel
+               end-read
+           end-perform.
+           
+           close Invoerbestand.
+           close Uitvoerbestand.
+           stop run.
+                        
