b7d09420248c267f7bbce22749bda2e12df1f9da
cluydts <jeroencluydts1@gmai.com>
2025-04-05 22:05:32 +0200

    changes to Salaris_berekening

--- Salaris_berekening.cbl
+++ Salaris_berekening.cbl
@@ -4,11 +4,19 @@
            data division.
            working-storage section.
            01 BRUTO-SALARIS pic 9(5)v99.
+           01 KEUZE pic 9(1).
+
            
 
 
            procedure division.
            
+           display "Bediende of Arbeider?".
+           display "1 - Bediende".
+           display "2 - Arbeider".
+           accept KEUZE.
+
+         
            
 
            end run.
