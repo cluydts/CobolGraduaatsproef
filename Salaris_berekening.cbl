27b2e184ad3905df970afd00fcca6e603c491d2c
cluydts <jeroencluydts1@gmai.com>
2025-04-05 22:01:50 +0200

    changes

---
+++ Salaris_berekening.cbl
@@ -0,0 +1,15 @@
+           identification division.
+           program-id. Salaris_berekening.
+
+           data division.
+           working-storage section.
+           01 BRUTO-SALARIS pic 9(5)v99.
+           
+
+
+           procedure division.
+           
+           
+
+           end run.
+           
