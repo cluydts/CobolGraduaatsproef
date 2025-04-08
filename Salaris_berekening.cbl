a972cb95d0eaa7ad81b12fc37f8b9dc28c7e5636
cluydts <jeroencluydts1@gmai.com>
2025-04-08 15:47:07 +0200

    Refactor indentation and correct tax calculation logic in SalarisBerekening

--- Salaris_berekening.cbl
+++ Salaris_berekening.cbl
@@ -1,15 +1,15 @@
-        IDENTIFICATION DIVISION.
-       PROGRAM-ID. SalarisBerekening.
+       IDENTIFICATION DIVISION.
+           PROGRAM-ID. SalarisBerekening.
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 Naam PIC X(30).
-       01 TypeWerknemer PIC X.
-       01 Brutoloon PIC 9(5)V99.
-       01 RSZ PIC 9(5)V99.
-       01 Voorheffing PIC 9(5)V99.
-       01 NettoLoon PIC 9(5)V99.
-
+           01 Naam PIC X(30).
+           01 TypeWerknemer PIC X.
+           01 Brutoloon PIC 9(5)V99.
+           01 RSZ PIC 9(5)V99.
+           01 Voorheffing PIC 9(5)V99.
+           01 NettoLoon PIC 9(5)V99.
+    
        PROCEDURE DIVISION.
            DISPLAY "Geef de naam van de werknemer:".
            ACCEPT Naam.
@@ -22,11 +22,15 @@
 
            COMPUTE RSZ = Brutoloon * 0.1307.
 
-        if brutoloon <= 15820
+           if  brutoloon <= 1318.33
             compute Voorheffing = brutoloon * 0.15
-        else if brutoloon > 15820 and brutoloon <= 27920
+           else if brutoloon > 1318.33 and brutoloon <= 2326.66
             compute Voorheffing = brutoloon * 0.25
-        else if brutoloon > 27920 and brutoloon 
+           else if brutoloon > 2326.66 and brutoloon <= 4026.66
+            compute voorheffing = brutoloon * 0.45
+            else if brutoloon > 4026.66
+            compute voorheffing = brutoloon * 0.50
+           end-if.
 
            COMPUTE NettoLoon = Brutoloon - RSZ - Voorheffing.
 
