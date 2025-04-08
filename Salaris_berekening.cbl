0ef4981b8a1a5cae8195c3a0601fffb0a7251448
cluydts <jeroencluydts1@gmai.com>
2025-04-08 15:38:36 +0200

    Refactor code structure for improved readability and maintainability

--- Salaris_berekening.cbl
+++ Salaris_berekening.cbl
@@ -1,23 +1,41 @@
-           identification division.
-           program-id. Salaris_berekening.
+        IDENTIFICATION DIVISION.
+       PROGRAM-ID. SalarisBerekening.
 
-           data division.
-           working-storage section.
-           01 BRUTO-SALARIS pic 9(5)v99.
-           01 KEUZE pic 9(1).
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 Naam PIC X(30).
+       01 TypeWerknemer PIC X.
+       01 Brutoloon PIC 9(5)V99.
+       01 RSZ PIC 9(5)V99.
+       01 Voorheffing PIC 9(5)V99.
+       01 NettoLoon PIC 9(5)V99.
 
-           
+       PROCEDURE DIVISION.
+           DISPLAY "Geef de naam van de werknemer:".
+           ACCEPT Naam.
 
+           DISPLAY "Type werknemer (B voor bediende, A voor arbeider):".
+           ACCEPT TypeWerknemer.
 
-           procedure division.
-           
-           display "Bediende of Arbeider?".
-           display "1 - Bediende".
-           display "2 - Arbeider".
-           accept KEUZE.
+           DISPLAY "Geef het brutoloon:".
+           ACCEPT Brutoloon.
 
-         
-           
+           COMPUTE RSZ = Brutoloon * 0.1307.
 
-           end run.
-           
+        if brutoloon <= 15820
+            compute Voorheffing = brutoloon * 0.15
+        else if brutoloon > 15820 and brutoloon <= 27920
+            compute Voorheffing = brutoloon * 0.25
+        else if brutoloon > 27920 and brutoloon 
+
+           COMPUTE NettoLoon = Brutoloon - RSZ - Voorheffing.
+
+           DISPLAY "----------------------------------------".
+           DISPLAY "Naam: " Naam.
+           DISPLAY "Bruto: " Brutoloon.
+           DISPLAY "RSZ: " RSZ.
+           DISPLAY "Voorheffing: " Voorheffing.
+           DISPLAY "Netto: " NettoLoon.
+           DISPLAY "----------------------------------------".
+
+           STOP RUN.
