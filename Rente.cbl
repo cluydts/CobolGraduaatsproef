d16c061d259d7f0012a61a146a693a3d37bbec49
cluydts <jeroencluydts1@gmai.com>
2025-04-10 12:21:16 +0200

    Refactor Rente.cbl for improved readability and maintainability

--- Rente.cbl
+++ Rente.cbl
@@ -24,38 +24,33 @@
           
            01  TOTAL-AMOUNT PIC 9(8)V99.
            01  CAPITAL PIC 9(8)V99.
-           01  RATE-OUT PIC 9(2)V99.
+           01  RATE PIC 9(2)V99.
            01  Years PIC 9(2).
            01  INTEREST PIC 9(8)V99.
 
 
        PROCEDURE DIVISION.
 
-              OPEN INPUT input-file
-                OPEN OUTPUT output-file
-                READ input-file INTO Leesregel
-                PERFORM UNTIL input-file = "EOF"
-                    READ input-file INTO input-record
-                        AT END
-                            MOVE "EOF" TO input-file
-                        NOT AT END
-                            UNSTRING Leesregel DELIMITED BY ","
-                                INTO CAPITAL RATE Years
-                            END-UNSTRING
-                            MOVE CAPITAL TO CAPITAL-OUT
-                            MOVE RATE TO RATE-OUT
-                            MOVE Years TO Years-OUT
-                        END-READ
+           OPEN INPUT input-file
+           OPEN OUTPUT output-file
+           READ input-file INTO Leesregel
+           PERFORM UNTIL input-file = "EOF"
+               READ input-file INTO input-record
+           AT END
+               MOVE "EOF" TO input-file
+           NOT AT END
+               UNSTRING Leesregel DELIMITED BY ","
+                   INTO CAPITAL RATE Years
+               END-UNSTRING
+    
+               MOVE function numval(CAPITAL) TO CAPITAL-OUT
+               MOVE function numval(RATE) TO RATE-OUT
+               MOVE function numval(Years) TO Years-OUT
+               COMPUTE INTEREST = (CAPITAL * RATE * Years) / 100.
+               COMPUTE TOTAL-AMOUNT = CAPITAL + INTEREST.
+           END-READ
 
-                    WRITE output-file FROM output-record
+           WRITE output-file FROM output-record
 
-                END-PERFORM.
-           
-
-           COMPUTE INTEREST = (CAPITAL * RATE * Years) / 100.
-           COMPUTE TOTAL-AMOUNT = CAPITAL + INTEREST.
-           
-
-          
-
+           END-PERFORM.
            STOP RUN.
