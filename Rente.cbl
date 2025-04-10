023e0c943d8ebec434d6911f61d3e5f02e4af2b7
cluydts <jeroencluydts1@gmai.com>
2025-04-10 15:29:31 +0200

    Refactor Rente.cbl to streamline variable handling and enhance output formatting

--- Rente.cbl
+++ Rente.cbl
@@ -15,67 +15,63 @@
             01 Leesregel pic x(80).
 
            FD output-file.
-           01  CAPITAL-OUT PIC X(10).
-           01  RATE-OUT PIC X(4).
-           01  Years-OUT PIC x(2).
-           01  INTEREST-OUT PIC x(10).
-
-           01 EOF-Flag PIC 9 VALUE 0.
-
-            01  CAPITAL-F PIC Z(8).99.
-           01  RATE-F PIC Z(2).99.
-        *>    01  Years-F PIC 9(2).
+           01 Output-Regel PIC X(80).
+           01 Header PIC X(21) value "Capital,Rate,Interest".
            01  INTEREST-F PIC Z(8).99.
+
+              01  EOF-Flag PIC X(1) VALUE "0".
 
               
        WORKING-STORAGE SECTION.
           
-           01  TOTAL-AMOUNT PIC 9(8)V99.
-           01  CAPITAL PIC x(10).
-           01  RATE PIC x(4).
-           01  Years PIC x(2).
+           01  TOTAL-AMOUNT-S PIC 9(8)V99.
+           01  CAPITAL-S PIC x(10).
+           01  RATE-S PIC x(4).
+           01  Years-S PIC x(2).
            01  INTEREST PIC 9(8)V99.
-
            01  CAPITAL-N PIC 9(8)V99.
            01  RATE-N PIC 9(2)V99.
            01  Years-N PIC 9(2).
-
-          
-
 
        PROCEDURE DIVISION.
 
            OPEN INPUT input-file
            OPEN OUTPUT output-file
            READ input-file INTO Leesregel
+
            PERFORM UNTIL EOF-Flag = 1
               read input-file INTO Leesregel
            AT END
                MOVE 1 to EOF-Flag
            NOT AT END
                UNSTRING Leesregel DELIMITED BY ","
-                   INTO CAPITAL RATE Years
+                   INTO CAPITAL-S RATE-S Years-S
                END-UNSTRING
     
-               MOVE function numval(CAPITAL) TO CAPITAL-N
-               MOVE function numval(RATE) TO RATE-N
-               MOVE function numval(Years) TO Years-N
+               MOVE function numval(CAPITAL-S) TO CAPITAL-N
+               MOVE function numval(RATE-S) TO RATE-N
+               MOVE function numval(Years-S) TO Years-N
+               
                COMPUTE INTEREST = (CAPITAL-N * RATE-N * Years-N) / 100
-               COMPUTE TOTAL-AMOUNT = CAPITAL-N + INTEREST
+               COMPUTE TOTAL-AMOUNT-S = CAPITAL-N + INTEREST
 
-                move CAPITAL-N to CAPITAL-F
-                move RATE-N to RATE-F
-                move Years-N to Years-out
-                move INTEREST to INTEREST-F
+               move INTEREST to INTEREST-F
 
-                write CAPITAL-F
-                write RATE-F
-                write Years-out
-                write INTEREST-F
+                string
+                      CAPITAL-S delimited by size
+                        "," delimited by size
+                        RATE-S delimited by size
+                        "," delimited by size
+                        Years-S delimited by size
+                        "," delimited by size
+                        INTEREST-F delimited by size
+                       into Output-Regel
 
+                       write Output-Regel
            END-READ
-
            END-PERFORM.
+
            close input-file.
-              close output-file.
+           close output-file.
+           
            STOP RUN.
